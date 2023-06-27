5b24a49bb709b0d775680fb5fcb4d82d74fe12d8
rodriguez-miguel-angel <rodriguez.miguel.angel@gmail.com>
2023-06-27 12:03:46 -0700

    Set up the menu API and the table booking API

---
+++ littlelemon/restaurant/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+
+from django.contrib.auth.models import User
+
+from .models import Booking, Menu
+
+
+"""
+version-01:
+"""
+class UserSerializer(ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+
+class MenuItemSerializer(ModelSerializer):
+    
+    class Meta:
+        model = Menu
+        fields = ['id', 'title', 'price', 'inventory']
+
+
+class BookingSerializer(ModelSerializer):
+    
+    class Meta:
+        model = Booking
+        fields = "__all__"
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,37 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+
+from rest_framework import generics
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+
+from rest_framework import viewsets
+from rest_framework.viewsets import ModelViewSet
+
+from rest_framework.permissions import IsAuthenticated
+
+from django.contrib.auth.models import User
+
+from .models import Booking, Menu
+from .serializers import MenuItemSerializer, BookingSerializer, UserSerializer
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemsView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
+   permission_classes = [IsAuthenticated] 
