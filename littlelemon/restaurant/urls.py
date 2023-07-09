31a2f7f2c47a1db7704194bfc82b26cda87ff7d0
rodriguez-miguel-angel <rodriguez.miguel.angel@gmail.com>
2023-07-09 13:05:31 -0700

    Refactor the backend and frontend.

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -8,10 +8,17 @@ app_name = 'restaurant'
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name="about"),
-    path('book/', views.book, name="book"),
-    path('reservations/', views.reservations, name="reservations"),
     path('menu', views.MenuItemsView.as_view(), name='menu'),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view(), name='menu_item'),
-    path('bookings', views.bookings, name='bookings'), 
+    # version-01:
+    # path('reservations/', views.reservations, name="reservations"),
+    path('reservations/', views.BookingViewSet.as_view({'get':'list'}), name='reservations'), 
+    
+    # version-01:
+    # path('book/', views.book, name="book"),
+    # path('bookings', views.bookings, name='bookings'),
+    path('book', views.book, name="book"),
+    path('booking', views.booking, name='booking'),
+    path('bookings', views.BookingViewSet.as_view({'post':'create'}), name="bookings"),
     path('api-token-auth/', obtain_auth_token),
 ]
